6d2e50b88480007375ae4fefa0f2c50737c8a41b
BRIZOU <59280@supinfo.com>
2014-03-25 14:38:16 +0100

    Commit branche

---
+++ src/org/sogeti/BeanMapper.java
@@ -0,0 +1,24 @@
+package org.sogeti;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.sogeti.bo.UserBean;
+
+import twitter4j.User;
+
+public class BeanMapper {
+	public static UserBean getUserBeanFromUser(User user) {
+		UserBean userBean = new UserBean();
+		try {
+			BeanUtils.copyProperties(userBean, user);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return userBean;
+	}
+}

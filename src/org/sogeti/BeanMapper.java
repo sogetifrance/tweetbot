a70a24edbeb51d7b55e451258f8f8a6e9aa878ae
BRIZOU <59280@supinfo.com>
2014-04-02 17:27:30 +0200

    ajout javadoc

--- src/org/sogeti/BeanMapper.java
+++ src/org/sogeti/BeanMapper.java
@@ -7,6 +7,12 @@ import org.sogeti.bo.UserBean;
 
 import twitter4j.User;
 
+
+/**
+ * Classe permettant le mapping entre User et UserBean
+ * @author FBRIZOU
+ *
+ */
 public class BeanMapper {
 	public static UserBean getUserBeanFromUser(User user) {
 		UserBean userBean = new UserBean();
